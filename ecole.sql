3ab0972073a51967e375c550d0ca120a2058c142
Momo <mohamedamin.mhadhbi355@gmail.com>
2025-11-06 11:51:32 +0100

    test

--- ecole.sql
+++ ecole.sql
@@ -4,14 +4,14 @@ use ecole,
 
 CREATE TABLE etudiants (
     id INT,
-    nom VARCHAR(),
-    prenom VARCHAR(100) NOT NULL,
+    nom VARCHAR(32),
+    prenom VARCHAR(32) NOT NULL,
     date_naissance DATE,
-    email VARCHAR(255) UNIQUE,
+    email VARCHAR(32) UNIQUE,
 );
 
 CREATE TABLE cours (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    code VARCHAR(20) NOT NULL UNIQUE,
-    titre VARCHAR(255) NOT NULL,
+    code VARCHAR(32) NOT NULL UNIQUE,
+    titre VARCHAR(32) NOT NULL,
 );
