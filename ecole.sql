04d23efc9598cd9872b16571f8e73421f8dc238b
Momo <mohamedamin.mhadhbi355@gmail.com>
2025-11-06 11:56:53 +0100

    test

--- ecole.sql
+++ ecole.sql
@@ -3,7 +3,7 @@ CREATE DATABASE ecole;
 use ecole,
 
 CREATE TABLE etudiants (
-    id INT,
+    id INT PRIMARY KEY auto_increment,
     nom VARCHAR(32),
     prenom VARCHAR(32) NOT NULL,
     date_naissance DATE,
@@ -15,4 +15,13 @@ CREATE TABLE cours (
     id INT,
     code VARCHAR(32) NOT NULL UNIQUE,
     titre VARCHAR(32) NOT NULL,
+);
+
+CREATE TABLE inscriptions (
+    id_etudiant INT NOT NULL,
+    id_cours INT NOT NULL,
+    date_inscription DATE,
+    PRIMARY KEY (id_etudiant, id_cours),
+    FOREIGN KEY (id_etudiant) REFERENCES etudiants(id),
+    FOREIGN KEY (id_cours) REFERENCES cours(id),
 );
